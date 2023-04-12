9dedbb72483d466e258e6bec4c0ee2f9086fe27d
Alex Mironov <alexan9610@gmail.com>
2023-04-12 23:32:33 +0300

    add php_lravel

--- .make_command/cp_env.sh
+++ .make_command/cp_env.sh
@@ -3,6 +3,10 @@ j=$#;
 while [ $i -le $j ]
 do
     cp .env $1/.env
+    if [ -f "$1/.env.example" ]; then
+        echo -e "\n" >> $1/.env;
+        cat $1/.env.example >> $1/.env;
+    fi;
     i=$((i + 1));
     shift 1;
 done
